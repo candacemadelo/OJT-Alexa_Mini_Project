fe25d2bbbb14b1e13e2000717f8722df47a93aa6
candacemadelo <62867453+candacemadelo@users.noreply.github.com>
2020-06-03 16:53:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,26 +16,57 @@ app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+//sets up the middleware for parsing the bodies and cookies off the requests
+app.use(bodyParser.json());
+app.use(cookieParse());
+
+// //function to check if the email address is valid
+// var isEmail = (email) => {
+// 	var emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+// 	if(typeof email != "string") {
+// 		return false;
+// 	}
+
+// 	return emailRegex.test(email);
+
+// };
+
 // home page
 app.get('/', function(req, res) {
 	res.render("");
 });
 
 //Login Page
-app.post("/login", function(req, res) {
-	if(err) {
-		console.log(err);
-	} else {
-		res.render("");
-	}
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //find user in database
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error();
+    }
+
+    //using bcrypt to compare passwords
+    const passwordValidated = await bcrypt.compare(password, user.password);
+    if (!passwordValidated) {
+      throw new Error();
+    }
+
+    res.render("");
+  } catch (err) {
+  	console.log(err);
+  }
 });
+
 
 // Registration page
 app.post("/register", function(req, res) {
 	User.create({
 		firstname: req.body.firstname,
 		lastname: req.body.lastname,
-		username: req.body.username,
+		email: req.body.email,
 		password: req.body.password
 	}, function(err, user) {
 		if(err) {
--- models/registration.js
+++ models/registration.js
@@ -1,10 +1,47 @@
 var mongoose = require("mongoose");
+var uniqueValidator = require('mongoose-unique-validator');
+var bcrypt = require("bcryptjs");
 
 var userSchema = new mongoose.Schema({
 	firstName: String,
 	lastName: String,
-	username: String,
-	password: String
+	email: {
+	    type: String,
+	    required: true,
+	    minlength: 1,
+	    trim: true, //get rid of whitespace
+	    unique: true, //note that the unique option is not a validator; we use mongoose-unique-validator to enforce it
+    },
+	password: {
+		type: String,
+		required : true,
+		minlength: 8
+	}
 });
 
-module.exports = mongoose.model("UserReg", userSchema);
+//emails will be unique
+userSchema.plugin(uniqueValidator);
+
+//this function will be called before a document is saved
+userSchema.pre('save', function(next) {
+  let user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  //hash generator for passwords
+  bcrypt
+    .genSalt(12)
+    .then((salt) => {
+      return bcrypt.hash(user.password, salt);
+    })
+    .then((hash) => {
+      user.password = hash;
+      next();
+    })
+    .catch((err) => next(err));
+});
+
+
+module.exports = mongoose.model("User", userSchema);
